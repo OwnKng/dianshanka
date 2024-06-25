fa30b132a650a1abc5f3a515446155aa670a111a
OwnKng <owen.king@outlook.com>
2024-06-25 23:08:33 +0100

    Improvements

--- main.go
+++ main.go
@@ -4,44 +4,70 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"ownkng.dev/cli/types"
+	"ownkng.dev/cli/input"
 	"ownkng.dev/cli/vocab"
 )
 
 const listHeight = 10
 
-type item struct {
-	title string
-	desc  string
+type keyMap struct {
+	Up    key.Binding
+	Down  key.Binding
+	Help  key.Binding
+	Quit  key.Binding
+	Enter key.Binding
 }
 
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.title }
-
-type Styles struct {
-	BorderColor lipgloss.Color
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Up, k.Down, k.Enter, k.Quit}
 }
 
-func DefaultStyles() *Styles {
-	s := new(Styles)
-	s.BorderColor = lipgloss.Color("#FF00FF")
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down},   // first column
+		{k.Help, k.Quit}, // second column
+	}
+}
 
-	return s
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Enter: key.NewBinding(
+		key.WithKeys("enter"),
+		key.WithHelp("enter", "select"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
 }
 
 type Main struct {
-	index    int
-	width    int
-	height   int
-	complete bool
-	styles   *Styles
-	list     list.Model
-	choice   string
-	game     types.Game
+	width  int
+	height int
+	keys   keyMap
+	help   help.Model
+	list   input.Input
+	choice string
+	game   vocab.Game
+	cursor int
+	reveal bool
 }
 
 func (m Main) Init() tea.Cmd {
@@ -49,72 +75,194 @@ func (m Main) Init() tea.Cmd {
 }
 
 func (m Main) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if m.game.Complete {
+		return m, tea.Quit
+	}
+
 	switch msg := msg.(type) {
 
 	//_ Handle resize events
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
+		m.help.Width = msg.Width
 
 	//_ Handle key events
 	case tea.KeyMsg:
 		switch msg.String() {
+
+		//* Help
+		case "?":
+			m.help.ShowAll = !m.help.ShowAll
+
+		//* Quit the application
 		case "q", "ctrl+c":
 
 			return m, tea.Quit
 
+		//* Handle key up and down - locked if in a reveal state
+		case "up", "k":
+			if m.cursor > 0 && !m.reveal {
+				m.cursor--
+				m.list.Down()
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.game.Rounds[m.game.Round].Cards)-1 && !m.reveal {
+				m.cursor++
+				m.list.Up()
+			}
+
+		//* Make a selection
 		case "enter":
 
-			return m, tea.Quit
+			//* If this is the second press
+			if m.reveal {
+				m.game.NextRound()
+
+				//* Update the inputs
+				items := []input.Item{}
+
+				for _, card := range m.game.Rounds[m.game.Round].Cards {
+					correct := false
+
+					if card.Chinese == m.game.Rounds[m.game.Round].Card.Chinese {
+						correct = true
+					}
+
+					items = append(items,
+						input.Item{Title: card.English, Subtitle: card.Pinyin, Value: card.Chinese, Correct: correct},
+					)
+				}
+
+				m.list = input.NewInput(items)
+				m.cursor = 0
+
+				//* Reset reveal
+				m.reveal = false
+
+				return m, nil
+			}
+
+			//* First press - reveal the correct answer
+			m.game.MarkAnswer(m.list.GetSelected().Value)
+			m.list.Reveal()
+			m.reveal = true
+
+			return m, nil
 		}
 	}
 
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
+	return m, nil
 }
 
+// Big heading
+var subduedColor = lipgloss.AdaptiveColor{Light: "#9B9B9B", Dark: "#5C5C5C"}
+
+var headingStyles = lipgloss.NewStyle().Padding(0, 1).Margin(0, 1, 0, 0).Background(lipgloss.Color("#5de4c7")).Foreground(lipgloss.Color("#000000"))
+var flashcardStyles = lipgloss.NewStyle().Padding(0, 1).Foreground(lipgloss.Color("#000000")).Background(lipgloss.Color("#ffffff"))
+var contentStyles = lipgloss.NewStyle().Margin(1, 0, 0, 1)
+
+var secondaryText = lipgloss.NewStyle().
+	Foreground(subduedColor)
+
 func (m Main) View() string {
-	current := m.game.Rounds[m.index]
+	current := m.game.Rounds[m.game.Round]
+
+	helpView := m.help.View(m.keys)
+
+	//* Headings
+	content := lipgloss.JoinVertical(
+		lipgloss.Left,
+		headingStyles.Render("你好!"),
+		lipgloss.NewStyle().Margin(1, 0, 0, 0).Width(50).Render("Welcome to 金闪卡, a flashy little terminal app to practise Chinese."),
+	)
+
+	if m.game.Complete {
+		score := m.game.GetScore() * 100
+		content += lipgloss.JoinVertical(
+			lipgloss.Left,
+			secondaryText.Margin(3, 0, 3, 0).Render(fmt.Sprintf("Game complete. You scored %.2f%% \n", score)),
+			helpView,
+		)
+
+		return lipgloss.Place(
+			m.width,
+			m.height,
+			lipgloss.Left,
+			lipgloss.Top,
+			contentStyles.Render(content),
+		)
+	}
+
+	cards := lipgloss.JoinHorizontal(
+		lipgloss.Left,
+		lipgloss.NewStyle().Padding(1, 0).Render(m.list.View()),
+	)
+
+	//* Cards
+	reveal := ""
+
+	if m.reveal {
+		reveal = lipgloss.JoinVertical(
+			lipgloss.Left,
+			lipgloss.JoinHorizontal(
+				lipgloss.Left,
+				flashcardStyles.MarginRight(2).Render(current.Card.Chinese),
+				flashcardStyles.MarginRight(2).Render(current.Card.Pinyin),
+				flashcardStyles.MarginRight(2).Render(current.Card.English),
+				fmt.Sprintln(""),
+			),
+		)
+	}
+
+	content = content + lipgloss.JoinVertical(
+		lipgloss.Left,
+		flashcardStyles.MarginTop(3).Render(current.Card.Chinese),
+		secondaryText.Margin(1, 0, 0, 0).Render("Select the correct English translation"),
+		cards,
+		reveal,
+		helpView,
+	)
 
 	return lipgloss.Place(
 		m.width,
 		m.height,
-		lipgloss.Center,
-		lipgloss.Center,
-		lipgloss.JoinVertical(
-			lipgloss.Left,
-			current.Character,
-			m.list.View(),
-		),
+		lipgloss.Left,
+		lipgloss.Top,
+		contentStyles.Render(content),
 	)
 }
 
 func main() {
-	game := vocab.NewGame(10)
+	game := vocab.NewGame(5)
 	round := game.Rounds[0]
 
-	items := []list.Item{
-		item{title: round.Cards[0].English, desc: round.Cards[0].Pinyin},
-		item{title: round.Cards[1].English, desc: round.Cards[1].Pinyin},
-		item{title: round.Cards[2].English, desc: round.Cards[2].Pinyin},
-		item{title: round.Cards[3].English, desc: round.Cards[3].Pinyin},
+	items := []input.Item{}
+
+	for _, card := range round.Cards {
+		correct := false
+
+		if card.Chinese == round.Card.Chinese {
+			correct = true
+		}
+
+		items = append(items,
+			input.Item{Title: card.English, Subtitle: card.Pinyin, Value: card.Chinese, Correct: correct},
+		)
 	}
 
-	l := list.New(items, list.NewDefaultDelegate(), 20, 20)
-	l.Title = "Select the matching meaning"
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
+	l := input.NewInput(items)
 
 	m := Main{
-		index:    0,
-		width:    20,
-		height:   listHeight,
-		complete: false,
-		styles:   DefaultStyles(),
-		list:     l,
-		choice:   "",
-		game:     game,
+		width:  20,
+		height: listHeight,
+		list:   l,
+		choice: "",
+		game:   game,
+		cursor: 0,
+		help:   help.New(),
+		keys:   keys,
 	}
 
 	programme := tea.NewProgram(m)
--- vocab/vocab.go
+++ vocab/vocab.go
@@ -5,13 +5,32 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-
-	"ownkng.dev/cli/types"
 )
 
-var Cards []types.Card
+type Game struct {
+	Cards          []Card
+	Complete       bool
+	NumberOfRounds int
+	Rounds         []Round
+	Score          int
+	Round          int
+}
 
-func readVocab() ([]types.Card, error) {
+type Round struct {
+	Card    Card
+	Cards   []Card
+	Correct bool
+}
+
+type Card struct {
+	Chinese string
+	Pinyin  string
+	English string
+}
+
+var Cards []Card
+
+func readVocab() ([]Card, error) {
 	basepath, _ := os.Getwd()
 	content, err := os.ReadFile(basepath + "/data/vocab.json")
 
@@ -19,7 +38,7 @@ func readVocab() ([]types.Card, error) {
 		return nil, fmt.Errorf("error reading file")
 	}
 
-	var vocab []types.Card
+	var vocab []Card
 	err = json.Unmarshal(content, &vocab)
 
 	if err != nil {
@@ -29,10 +48,10 @@ func readVocab() ([]types.Card, error) {
 	return vocab, nil
 }
 
-func NewGame(rounds int) types.Game {
+func NewGame(rounds int) Game {
 	cards, _ := readVocab()
 
-	game := types.Game{NumberOfRounds: rounds, Cards: cards, Complete: false, Score: 0}
+	game := Game{NumberOfRounds: rounds, Cards: cards, Complete: false, Score: 0, Round: 0}
 
 	for i := 0; i < rounds; i++ {
 		newRound(&game)
@@ -41,14 +60,8 @@ func NewGame(rounds int) types.Game {
 	return game
 }
 
-func randomCharacter(g *types.Game) string {
-	index := rand.Intn(len(g.Cards))
-
-	return g.Cards[index].Chinese
-}
-
-func randomCards(g *types.Game) []types.Card {
-	var cards []types.Card
+func randomCards(g *Game) []Card {
+	var cards []Card
 
 	for i := 0; i < 4; i++ {
 		index := rand.Intn(len(g.Cards))
@@ -58,35 +71,38 @@ func randomCards(g *types.Game) []types.Card {
 	return cards
 }
 
-func newRound(g *types.Game) {
+func newRound(g *Game) {
 	cards := randomCards(g)
 
-	r := types.Round{Character: cards[0].Chinese, Cards: cards}
+	index := rand.Intn(len(cards))
+
+	r := Round{Card: cards[index], Cards: cards}
 
 	g.Rounds = append(g.Rounds, r)
 }
 
-func CheckAnswer(r *types.Round, c string) {
-	if r.Character == c {
-		r.Correct = true
+func (g *Game) MarkAnswer(c string) {
+	if g.Rounds[g.Round].Card.Chinese == c {
+		g.Rounds[g.Round].Correct = true
 	} else {
-		r.Correct = false
+		g.Rounds[g.Round].Correct = false
 	}
 }
 
-func NextRound(g *types.Game) {
-	if g.Round < g.NumberOfRounds {
-		newRound(g)
+func (g *Game) NextRound() {
+	if g.Round < g.NumberOfRounds-1 {
 		g.Round++
 	} else {
 		g.Complete = true
 	}
 }
 
-func UpdateScore(g *types.Game) {
+func (g *Game) GetScore() float32 {
 	for _, round := range g.Rounds {
 		if round.Correct {
 			g.Score++
 		}
 	}
+
+	return float32(g.Score) / float32(g.NumberOfRounds)
 }
